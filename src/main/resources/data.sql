c73bbab652b68dff65ba3c146196b5706b683656
abisha <abishasherlin@arffy.com>
2024-01-25 13:04:53 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+
+insert into alien(id,name,tech) values(101,'abisha','java');
+insert into alien(id,name,tech) values(102,'arul','python');
+insert into alien(id,name,tech) values(103,'eunice','cloud');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+
+create table alien(
+     id int primary key,
+     name varchar(20),
+     tech varchar(20)
+
+);
